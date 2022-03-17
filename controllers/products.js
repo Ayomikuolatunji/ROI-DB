ce5b9d93f77e8bc211d9cd6d53826abc97ffaedc
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-03-17 22:25:46 +0100

    connecting database

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,15 @@
 
 
 const getProducts=(req,res,next)=>{
-    res.status(200).send("all item")
+    res.status(200).json(req.body)
 }
 
 const getSingleProduct=(req,res,next)=>{
-    res.status(200).send("good")
+    res.status(200).json(req.body)
 }
 
 const postProduct=(req,res,next)=>{
-    res.status(201).send("pots a single poroducts")
+    res.status(201).json(req.body)
 }
 
 const updateProducts=(req,res,next)=>{
