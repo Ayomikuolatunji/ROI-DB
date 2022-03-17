f563758e7e97f2e591f1112cdc7da3c88e5b15dd
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-03-18 00:42:07 +0100

    connecting database mongoose query

--- controllers/products.js
+++ controllers/products.js
@@ -32,10 +32,25 @@ const createProduct=async(req,res,next)=>{
 }
 
 const updateProducts=async(req,res,next)=>{
+    try {
+        
+    } catch (error) {
+        res.status(500).json({errMsg:error})
+    }
      res.status(200).send("update a product")
 }
 
 const deleteProduct=async(req,res,next)=>{
+    try {
+        const {id:prodId}=req.params;
+        const deleteProd=await ProductSchema.findOneAndDelete({_id:prodId});
+        if(!deleteProd){
+            return res.status(404).json({errMsg:`No item found with the ${prodId}`})
+        }
+        res.status(200).json(deleteProd)
+    } catch (error) {
+        res.status(500).json({errMsg:error})
+    }
     res.status(200).send("delete a product")
 }
 module.exports={getProducts,getSingleProduct,createProduct,updateProducts,deleteProduct}
