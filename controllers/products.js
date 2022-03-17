5284438ebba4af1064ca7c60bcdcc6bb360f6d8b
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-03-18 00:22:18 +0100

    connecting database mongoose query

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,29 @@
 const ProductSchema=require("../models/product")
 
-const getProducts=(req,res,next)=>{
-    res.status(200).json(req.body)
+const getProducts=async(req,res,next)=>{
+    try {
+        const products=await ProductSchema.find({});
+        res.status(200).json(products)
+    } catch (error) {
+        throw new Error("no products found")
+    }
 }
 
 const getSingleProduct=(req,res,next)=>{
-    res.status(200).json(req.body)
+    try {
+        
+    } catch (error) {
+        
+    }
 }
 
 const createProduct=async(req,res,next)=>{
-    const products= await ProductSchema.create(req.body)
-    res.status(201).json(products)
+    try {
+        const products= await ProductSchema.create(req.body)
+        res.status(201).json(products) 
+    } catch (error) {
+        res.status(500).json({errMsg:error})
+    }
 }
 
 const updateProducts=(req,res,next)=>{
