e253170594112786c31aacfa99db84c2b4f6ca59
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-03-18 01:31:24 +0100

    all method api now working

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-
 const express=require("express");
 const app=express()
 const port=5000;
 const {router}=require("./routes/products");
-const {connectFunc}=require("./db/connect")
+const {connectFunc}=require("./db/connect");
 
 require('dotenv').config()
-
 
 app.use(express.json())
 app.use("/api/v2/products",router)
